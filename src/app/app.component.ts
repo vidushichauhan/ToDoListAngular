b3da6fe66811832d1ae50bcc54c0516f3aa4991e
vidushichauhan <vidushic0@gmail.com>
2021-06-19 18:30:47 +0530

    Added navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cwh-todo-list';
+  constructor(){
+    // setTimeout(() => {
+    //   this.title="Changed Title"
+    // }, 2000);
+    
+  }
 }
